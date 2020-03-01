643f1cf50a4ec6aeb598880c706427ac07124005
GGOOTEE <59986488+GGOOTEE@users.noreply.github.com>
2020-03-01 12:21:03 -0500

    Particles and screen shake

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -1,9 +1,26 @@
 extends Node2D
 
-onready var Camera = $Camera
+var x = 0
+var y = 0
 
 func _ready():
-	pass
+	set_process_input(true)
 
-func _physics_process(delta):
-	pass
+
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.is_action_pressed("ui_left"): x -= 5
+		$Camera.add_trauma(0.1)
+		if event.is_action_pressed("ui_right"): x += 5
+		$Camera.add_trauma(0.1)
+		if event.is_action_pressed("ui_up"): y += 5
+		$Camera.add_trauma(0.1)
+		if event.is_action_pressed("ui_down"): y -= 5
+		$Camera.add_trauma(0.1)
+		print(str(x))
+
+
+onready var Camera = $Camera
+
+#func _ready():
+	#pass
